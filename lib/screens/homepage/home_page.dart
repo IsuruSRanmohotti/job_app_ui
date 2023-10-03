e96e1832f45777f4ab53008424ae2ecb559190d1
IsuruSRanmohotti <isurusampathranmohotti@gmail.com>
2023-10-03 21:14:57 +0530

    first commit

---
+++ lib/screens/homepage/home_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppBar(),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Good Morning Alexa",
+                style: TextStyle(color: Colors.grey, fontSize: 17),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Find Your\nCreative Job",
+                style: TextStyle(
+                  fontSize: 32,
+                  height: 0.9,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: size.width * 0.75,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: UnderlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12)),
+                          hintText: "Search For Jobs",
+                          fillColor: Colors.white,
+                          filled: true),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 55,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Icon(
+                      Icons.search_rounded,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
